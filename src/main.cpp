614c3a9800bd1ccf1a9fd3fc8c4de59f9ee44282
David de Prez <dosprez@avans.nl>
2017-04-22 15:36:40 +0200

    UART/SPI incomplete test in main
    ADD			test code in main (with + without interrupt)
    
    MESSAGE:
    is miss a communication to outside for succeed/failure.

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,45 @@
 #include <SPI.h>
 #include <UART.h>
+#include <config_file.h>
 #include "stm32f4_discovery.h"
 #include "stm32f4xx_conf.h"
 
+void delay(uint32_t time);
+
 int main(void)
 {
-	volatile int i = 1;
-	SPI comm;
+	volatile uint8_t i = 0x55;
+	uint16_t buffer;
+#if SPI_INTERRUPT_ENABLE
+	SPI spiInstance = SPI::sInstance;
+#else
+	SPI spiInstance;
+#endif
+#if UART_INTERRUPT_ENABLE
+	UART uartInstance = UART::sInstance;
+#else
+	UART uartInstance;
+#endif
 
 	while(i){
-		comm.write(7);
+		spiInstance.write(i);
+		uartInstance.write(i);
+		delay(0x3FFFFF);
+
+		buffer = uartInstance.read();
+		if (buffer == i) {
+			//green
+		} else if (buffer > i) {
+			//other than previous
+		} else if (buffer < i){
+			//other then previous
+		}
+	}
+}
+
+void delay(uint32_t time){
+	//block for time
+	while(time--) {
+		asm volatile ("nop");
 	}
 }
